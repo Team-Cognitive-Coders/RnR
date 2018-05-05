e72e96d9aeb81fc8441278395c070baf6a3efc3b
Mohammed Jawad Sonalkar <mohammed.jawad.sonalkar@sap.com>
2018-05-05 14:15:55 +0530

    Connect with API.ai

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,33 +1,35 @@
-jQuery(document).ready(function () {
+(function () {
+    var payload = {
 
+    };
+    var client;
+    function setAccessToken(token){
+        client = new ApiAi.ApiAiClient({accessToken: token});
+    }
+    setAccessToken("aaa1e59cf7ec4f5dbf5182e0cc754a59");
     $(".messages").animate({ scrollTop: $(document).height() }, "fast");
-
-    // $.ajax({
-    //     url : "/startChat/118809975300669",
-    //     type : "GET",
-    //     success : function(d){
-    //     }
-    // });
 
     function newMessage() {
         message = $(".message-input input").val();
         if ($.trim(message) == '') {
             return false;
         }
-        $.ajax({
-            url : "/sendMessage",
-            type : "POST",
-            data : {"message" : message},
-            dataType: "json",
-            success : function(d){
-                $('<li class="sent"><img src="http://emilcarlsson.se/assets/mikeross.png" alt="" /><p>' + message + '</p></li>').appendTo($('.messages ul'));
-                $('.message-input input').val(null);
-                $('.contact.active .preview').html('<span>You: </span>' + message);
-                $(".messages").animate({ scrollTop: $(document).height() }, "fast");
-            }
-        });
+        addUserMessageToUI(message);
+        sendUserMessage(message);
     };
+    function sendUserMessage(message){
+        client.textRequest(message).then(function(response){
+            var result;
+            try {
+                result = response.result.fulfillment.speech
+            } catch(error) {
+                result = "";
+            }
+            addBotMessageToUI(result);
+        }).catch(function(){
 
+        });
+    }
     $('.submit').click(function () {
         newMessage();
     });
@@ -39,16 +41,16 @@ jQuery(document).ready(function () {
         }
     });
 
-    addUserMessageToUI = function(message){
+    function addUserMessageToUI(message){
         $('<li class="sent"><img src="http://emilcarlsson.se/assets/mikeross.png" alt="" /><p>' + message + '</p></li>').appendTo($('.messages ul'));
         $('.message-input input').val(null);
         $('.contact.active .preview').html('<span>You: </span>' + message);
         $(".messages").animate({ scrollTop: $(document).height() }, "fast");
     };
-    addBotMessageToUI = function(message){
+    function addBotMessageToUI(message){
         $('<li class="replies"><img src="http://emilcarlsson.se/assets/mikeross.png" alt="" /><p>' + message + '</p></li>').appendTo($('.messages ul'));
         $('.message-input input').val(null);
         $('.contact.active .preview').html('<span>You: </span>' + message);
         $(".messages").animate({ scrollTop: $(document).height() }, "fast");
     };
-});
+})();
